af84c7eda8c57ff86900de12e50d7f5dddbfc059
MantsG <pmantas05@gmail.com>
2025-09-25 18:37:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 
 using std::cout;
 using std::cin;
@@ -15,6 +18,7 @@ using std::right;
 using std::fixed;
 using std::setprecision;
 using std::sort;
+using std::ifstream;
 
 struct Studentas{
     string var;
@@ -36,17 +40,69 @@ double median(vector<int> &v){
         return (laik[n/2-1] + laik[n/2]) / 2.0;
     }
 
-Studentas Stud_iv();
+Studentas Stud_iv(bool atsitiktinis);
+
+vector<Studentas> SkaitytiFaila(const string &failoVardas){
+    vector<Studentas> grupe;
+    ifstream in(failoVardas);
+    if(!in){
+        cout<<"Nepavyko atidaryti failo"<<endl;
+        return grupe;
+    }
+    string vard, pavarde;
+    string header;
+    std::getline(in, header);
+
+    while(in >> vard >> pavarde){
+        Studentas s;
+        s.var = vard;
+        s.pav = pavarde;
+        s.nd.clear();
+
+        int paz;
+        vector<int> laik;
+        while(in.peek() != '\n' && in >> paz){
+            laik.push_back(paz);
+        }
+        if(!laik.empty()){
+            s.egz = laik.back();
+            laik.pop_back();
+            s.nd = laik;
+
+            double vid = 0;
+            for(int x: s.nd) vid += x;
+            if(!s.nd.empty()) vid /= s.nd.size();
+            s.galVid = vid*0.4 + 0.6*s.egz;
+            s.GalMed = median(s.nd)*0.4 + 0.6*s.egz;
+        }
+        grupe.push_back(s);
+    }
+    return grupe;
+}
 
 int main(){
-
+    srand(time(0));
     vector <Studentas> Grupe;
-    cout<<"Kiek studentu grupeje? ";
-    int m;
-    cin>>m;
+    
+    cout<<"Pasirinkite duomenu isvedimo buda: "<<endl;
+    cout<<"1 - Ivesti ranka"<<endl;
+    cout<<"2 - Generuoti atsitiktinai"<<endl;
+    cout<<"3 - Nuskaityti is failo"<<endl;
 
-    for(auto z=0; z<m; z++){
-        Grupe.push_back(Stud_iv());
+    int ivBudas;
+    cin>>ivBudas;
+
+    if(ivBudas == 3){
+        Grupe = SkaitytiFaila("kursiokai.txt");
+    }
+    else{
+        cout<<"Kiek studentu grupeje? ";
+        int m;
+        cin>>m;
+        bool atsitiktinis = (ivBudas == 2);
+        for(auto z=0; z<m; z++){
+        Grupe.push_back(Stud_iv(atsitiktinis));
+        }
     }
     cout<<"Pasirinkite galutinio balo skaiciavimo buda: "<<endl;
     cout<<"1 - Vidurkis"<<endl;
@@ -71,32 +127,43 @@ int main(){
         }
     }
 
-    Studentas Stud_iv(){
+    Studentas Stud_iv(bool atsitiktinis){
         int laik_nd, sum=0;
 
         Studentas Pirmas;
         cout<<"Ivesk studento duomenis"<<endl;
         cout<<"Vardas: "; cin>>Pirmas.var;
         cout<<"Pavarde: "; cin>>Pirmas.pav;
-        cout<<"Iveskite studento ("<<Pirmas.var<<" "<<Pirmas.pav<<") namu darbu pazymius (Paspauskite enter 2 kartus, kad baigti): "<<endl;
-        while(true){
-            cout<<Pirmas.nd.size()+1<<": ";
-            cin>>laik_nd;
-            if(laik_nd == 0) break;
-            if(laik_nd < 1 || laik_nd > 10){
-                cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10 arba 0 pabaigai"<<endl;
-                continue;
-            }
-            Pirmas.nd.push_back(laik_nd);
-            sum+=laik_nd;
+            if (atsitiktinis){
+                int nd_kiekis = rand()%10 + 1;
+                for(int i=0; i<nd_kiekis; i++){
+                    laik_nd = rand()%10 + 1;
+                    Pirmas.nd.push_back(laik_nd);
+                    sum+=laik_nd;
         }
-
-        cout<<"Studento egzamino pazimys: ";
-        while(!(cin>>Pirmas.egz) || Pirmas.egz < 1 || Pirmas.egz > 10){
+        Pirmas.egz = rand()%10 + 1;
+        cout<<"Sugeneruoti "<<nd_kiekis<<" namu darbu pazymiai ir egzaminas"<<endl;
+    }
+        else{
+            cout<<"Iveskite studento("<<Pirmas.var<<" "<<Pirmas.pav<<") namu darbu pazymius (Paspauskite enter 2 kartus, kad baigti): "<<endl;
+            while (true){
+                cout<<Pirmas.nd.size()+1<<": ";
+                cin>>laik_nd;
+                if(laik_nd == 0) break;
+                if(laik_nd < 1 || laik_nd > 10){
+                    cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10"<<endl;
+                    continue;
+                }
+                Pirmas.nd.push_back(laik_nd);
+                sum+=laik_nd;
+            }
+            cout<<"Studento egzamino pazimys: ";
+            while(!(cin>>Pirmas.egz) || Pirmas.egz < 1 || Pirmas.egz > 10){
             cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10"<<endl;
             cin.clear();
             cin.ignore(10000, '\n');
             cout<<"Studento egzamino pazimys: ";
+            }
         }
 
         if(!Pirmas.nd.empty()){
@@ -109,5 +176,6 @@ int main(){
     }
             return Pirmas;
     }
+
 
 
