4477047a98bd522a5a05da170f1d052e5a60a351
MantsG <pmantas05@gmail.com>
2025-09-25 16:28:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
-#include <sort>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -29,7 +29,7 @@ double median(vector<int> &v){
     if(v.empty()) return 0.0;
     vector<int> laik=v;
     sort(laik.begin(), laik.end());
-    int n = laik.size()
+    int n = laik.size();
     if(n % 2 ==1)
         return laik[n/2];
     else
@@ -48,44 +48,66 @@ int main(){
     for(auto z=0; z<m; z++){
         Grupe.push_back(Stud_iv());
     }
-    cout<<setw(10)<<left<<"Vardas"<<setw(15)<<left<<"Pavarde"<<setw(16)<<left<<"Galutinis (Vid.)"<<endl;
-    cout<<"-----------------------------------------"<<endl;
+    cout<<"Pasirinkite galutinio balo skaiciavimo buda: "<<endl;
+    cout<<"1 - Vidurkis"<<endl;
+    cout<<"2 - Mediana"<<endl;
+    cout<<"3 - abu"<<endl;
+    int pasirinkimas;
+    cin>>pasirinkimas;
+
+    cout<<setw(10)<<left<<"Vardas"<<setw(15)<<left<<"Pavarde";
+
+    if(pasirinkimas == 1) cout<<setw(16)<<left<<"Galutinis (Vid.)"<<endl;
+    else if(pasirinkimas == 2) cout<<setw(16)<<left<<"Galutinis (Med.)"<<endl;
+    else cout<<setw(18)<<left<<"Galutinis (Vid.)"<<setw(16)<<left<<"Galutinis (Med.)"<<endl;
+
+    cout<<"-----------------------------------------------------------"<<endl;
+    
     for(auto Past:Grupe){
-        cout<<setw(10)<<left<<Past.var<<setw(15)<<left<<Past.pav<<setw(16)<<left<<fixed<<setprecision(2)<<Past.gal<<endl;
+        cout<<setw(10)<<left<<Past.var<<setw(15)<<left<<Past.pav;
+        if(pasirinkimas == 1) cout<<setw(16)<<left<<fixed<<setprecision(2)<<Past.galVid<<endl;
+        else if(pasirinkimas == 2) cout<<setw(16)<<left<<fixed<<setprecision(2)<<Past.galMed<<endl;
+        else cout<<setw(18)<<left<<fixed<<setprecision(2)<<Past.galVid<<setw(16)<<left<<fixed<<setprecision(2)<<Past.galMed<<endl;
         }
     }
 
     Studentas Stud_iv(){
-        int n, laik_nd, sum=0;
+        int laik_nd, sum=0;
 
         Studentas Pirmas;
         cout<<"Ivesk studento duomenis"<<endl;
         cout<<"Vardas: "; cin>>Pirmas.var;
         cout<<"Pavarde: "; cin>>Pirmas.pav;
-        cout<<"Kiek pazymiu gavo studentas uz namu darbus: "<<Pirmas.var<<" "<<Pirmas.pav<<": ";cin>>n;
-
-        for(int i=0; i<n; i++){
-                cout<<i+1<<": ";
-                while(!(cin>>laik_nd)|| laik_nd < 1 || laik_nd >10){
-                    cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10.\n";
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout<<i+1<<": ";
-                }
-
+        cout<<"Iveskite studento ("<<Pirmas.var<<" "<<Pirmas.pav<<") namu darbu pazymius (Paspauskite enter 2 kartus, kad baigti): "<<endl;
+        while(true){
+            cout<<Pirmas.nd.size()+1<<": ";
+            cin>>laik_nd;
+            if(laik_nd == 0) break;
+            if(laik_nd < 1 || laik_nd > 10){
+                cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10 arba 0 pabaigai"<<endl;
+                continue;
+            }
             Pirmas.nd.push_back(laik_nd);
             sum+=laik_nd;
         }
 
         cout<<"Studento egzamino pazimys: ";
         while(!(cin>>Pirmas.egz) || Pirmas.egz < 1 || Pirmas.egz > 10){
-            cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10.\n";
+            cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10"<<endl;
             cin.clear();
             cin.ignore(10000, '\n');
             cout<<"Studento egzamino pazimys: ";
         }
-        Pirmas.gal=double(sum)/double(n)*0.4+0.6*Pirmas.egz;
 
-        return Pirmas;
+        if(!Pirmas.nd.empty()){
+            double vid = double(sum)/double(Pirmas.nd.size());
+            Pirmas.galVid=vid*0.4+0.6*Pirmas.egz;
+            Pirmas.galMed=median(Pirmas.nd)*0.4+0.6*Pirmas.egz;
+        }   else{
+            Pirmas.galVid=0.6*Pirmas.egz;
+            Pirmas.galMed=0.6*Pirmas.egz;
     }
+            return Pirmas;
+    }
+
 
