a4264027b52f52177dd390704e77f979801aae62
MantsG <pmantas05@gmail.com>
2025-10-09 18:28:17 +0300

    Create Studentas.h

---
+++ Studentas.h
@@ -0,0 +1,16 @@
+#ifndef STUDENTAS_H_INCLUDED
+#define STUDENTAS_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+struct Studentas{
+    std::string var;
+    std::string pav;
+    std::vector <int> nd;
+    int egz;
+    double galVid;
+    double galMed;
+};
+
+#endif
