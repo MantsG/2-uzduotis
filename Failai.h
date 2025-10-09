e2c73e4b97afb7ee9a63e437746800e36ba8b94d
MantsG <pmantas05@gmail.com>
2025-10-09 18:27:34 +0300

    Create Failai.h

---
+++ Failai.h
@@ -0,0 +1,11 @@
+#ifndef FAILAI_H_INCLUDED
+#define FAILAI_H_INCLUDED
+
+#include <string>
+#include <vector>
+#include "Studentas.h"
+
+void GeneruotiFaila(const std::string& failoVardas, int kiek);
+std::vector<Studentas> SkaitytiFaila(const std::string &failoVardas);
+
+#endif
