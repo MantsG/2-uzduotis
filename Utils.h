3a8e0ec2ba00ebb35a78b4ca130e2b38048d5754
MantsG <pmantas05@gmail.com>
2025-10-09 18:32:11 +0300

    Create Utils.h

---
+++ Utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+#include <string>
+#include <vector>
+#include "Studentas.h"
+
+bool naturalCompare(const std::string& a, const std::string& b);
+double median(std::vector<int> &v);
+
+#endif
