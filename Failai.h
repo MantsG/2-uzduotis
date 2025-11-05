66059b60a9e60e70e5fca9021c2324013c9370f6
MantsG <pmantas05@gmail.com>
2025-11-05 14:51:33 +0200

    Update Failai.h

--- Failai.h
+++ Failai.h
@@ -2,10 +2,10 @@
 #define FAILAI_H_INCLUDED
 
 #include <string>
-#include <vector>
 #include "Studentas.h"
 
 void GeneruotiFaila(const std::string& failoVardas, int kiek);
-std::vector<Studentas> SkaitytiFaila(const std::string &failoVardas);
+Konteineris SkaitytiFaila(const std::string &failoVardas);
+
 
 #endif
