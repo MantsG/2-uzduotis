ddad3e212f3560393534b6bcaa1bc452cdcd5cac
MantsG <pmantas05@gmail.com>
2025-11-14 10:45:17 +0200

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -24,3 +24,14 @@ private:
     double galVid_;
     double galMed_;
 };
+
+public:
+    Studentas() : egzaminas_(0), galVid_(0), galMed_(0) {}
+    Studentas(std::istream& is) {readStudent(is);}
+    
+    inline string vardas() const {return vardas_;}
+    inline string pavarde() const {return pavarde_;}
+    inline double galVid() const {return galVid_;}
+    inline double galMed() const {return galMed_;}
+    inline int egzaminas() const {return egzaminas_;}
+    inline const vector<int>& nd() const {return nd_;}
