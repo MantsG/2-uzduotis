325ee1d47d92da511988b2c31895bfb9e58e8115
MantsG <pmantas05@gmail.com>
2025-11-14 11:06:39 +0200

    Update Studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -1,2 +1,8 @@
 #include "Studentas.h"
 #include <iomanip>
+
+double Studentas::galBalas(double (*skaiciaviams)(std::vector<int>&)) const{
+    if (nd_.empty()) return 0.6 * egzaminas_;
+    std::vector<int> kopija = nd_;
+    return skaiciavimas(kopija) * 0.4 + 0.6 * egzaminas_;
+}
