2fcb02b83ba0fa031704a17bfef61cae2ffa70d9
MantsG <pmantas05@gmail.com>
2025-11-14 11:27:17 +0200

    Update Ivedimas.cpp

--- Ivedimas.cpp
+++ Ivedimas.cpp
@@ -20,29 +20,35 @@ Studentas Stud_iv(bool atsitiktinis){
 
     Studentas Pirmas;
     cout<<"Ivesk studento duomenis"<<endl;
-    cout<<"Vardas: "; cin>>Pirmas.var;
-    cout<<"Pavarde: "; cin>>Pirmas.pav;
-
+    string vardas, pavarde;
+    cout<<"Vardas: "; cin>>vardas;
+    cout<<"Pavarde: "; cin>>pavarde;
+    Pirmas.setVardas(vardas);
+    Pirmas.setPavarde(pavarde);
+    
     if(atsitiktinis){
         int nd_kiekis = rand()%10 + 1;
+        std::vector<int> tmp;
         for(int i=0; i<nd_kiekis; i++){
             laik_nd = rand()%10 + 1;
-            Pirmas.nd.push_back(laik_nd);
+            Pirmas.pridetiNd(laik_nd);
             sum+=laik_nd;
         }
-        Pirmas.egz = rand()%10 + 1;
+        Pirmas.setNd(tmp);
+        Pirmas.setEgzaminas(rand()%10 + 1);
+        Pirmas.skaiciuotiGalutinis();
         cout<<"Sugeneruoti "<<nd_kiekis<<" namu darbu pazymiai ir egzaminas"<<endl;
     }
     else{
         string ndStr;
         cin.ignore();
 
-        cout<<"Iveskite studento ("<<Pirmas.var<<" "<<Pirmas.pav<<") namu darbu pazymius (paspauskite 2 kartus ENTER, kad baigti): "<<endl;
+        cout<<"Iveskite studento ("<<Pirmas.vardas()<<" "<<Pirmas.pavarde()<<") namu darbu pazymius (paspauskite 2 kartus ENTER, kad baigti): "<<endl;
 
         int enter = 0;
 
         while(true){
-            cout<<Pirmas.nd.size()+1<<": ";
+            cout<<Pirmas.nd().size()+1<<": ";
             std::getline(cin, ndStr);
 
             if(ndStr.empty()){
@@ -55,7 +61,7 @@ Studentas Stud_iv(bool atsitiktinis){
             if(all_of(ndStr.begin(), ndStr.end(), ::isdigit)){
                 laik_nd = stoi(ndStr);
                 if(laik_nd >= 1 && laik_nd <= 10){
-                    Pirmas.nd.push_back(laik_nd);
+                    Pirmas.addNd(laik_nd);
                     sum += laik_nd;
                     continue;
                 }
@@ -68,21 +74,20 @@ Studentas Stud_iv(bool atsitiktinis){
             cout<<"Studento egzamino pazimys: ";
             cin>>egzStr;
             if(!egzStr.empty() && all_of(egzStr.begin(), egzStr.end(), ::isdigit)){
-                Pirmas.egz = stoi(egzStr);
-                if(Pirmas.egz >= 1 && Pirmas.egz <= 10) break;
+                Pirmas.setEgzaminas(stoi(egzStr));
+                if(Pirmas.egzaminas() >= 1 && Pirmas.egzaminas() <= 10) break;
             }
             cout<<"Neteisinga ivestis, iveskite skaiciu nuo 1 iki 10"<<endl;
         }
+        Pirmas.skaiciuotiGalutinis();
     }
 
 
-    if(!Pirmas.nd.empty()){
-        double vid = double(sum)/double(Pirmas.nd.size());
-        Pirmas.galVid=vid*0.4+0.6*Pirmas.egz;
-        Pirmas.galMed=median(Pirmas.nd)*0.4+0.6*Pirmas.egz;
+    if(!Pirmas.nd().empty()){
+        double vid = double(sum)/double(Pirmas.nd().size());
+        Pirmas.skaiciuotiGalutinis();
     } else{
-        Pirmas.galVid=0.6*Pirmas.egz;
-        Pirmas.galMed=0.6*Pirmas.egz;
+        Pirmas.skaiciuotiGalutinis();
     }
     return Pirmas;
 }
