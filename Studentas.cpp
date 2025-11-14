dd954f1d6cf851c5c764daedbddf38286de482a9
MantsG <pmantas05@gmail.com>
2025-11-14 11:08:46 +0200

    Update Studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -6,3 +6,16 @@ double Studentas::galBalas(double (*skaiciaviams)(std::vector<int>&)) const{
     std::vector<int> kopija = nd_;
     return skaiciavimas(kopija) * 0.4 + 0.6 * egzaminas_;
 }
+
+std::istream& Studentas::readStudent(std::istream& is){
+    is>>vardas_>>pavarde_;
+    nd_.clear();
+    int paz;
+    for(int i=0; i<10; i++){
+        if(!(is>>paz)) break;
+        nd_.push_back(paz);
+    }
+    is>>egzaminas_;
+    skaiciuotiGalutinis();
+    return is;
+}
