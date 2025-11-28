ddc11cf2843bfd7f06cecff15d1b36ec5a179ab2
MantsG <pmantas05@gmail.com>
2025-11-28 19:15:48 +0200

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -29,6 +29,10 @@ public:
     Studentas() : egzaminas_(0), galVid_(0), galMed_(0) {}
     Studentas(std::istream& is) {readStudent(is);}
     
+    ~Studentas();
+    Studentas(const Studentas& other);
+    Studentas& operator=(const Studentas& other);
+
     inline std::string vardas() const {return vardas_;}
     inline std::string pavarde() const {return pavarde_;}
     inline double galVid() const {return galVid_;}
