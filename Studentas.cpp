0c2d1d27b09152531af2be48e455ae16d5e6afd1
MantsG <pmantas05@gmail.com>
2025-12-04 16:52:20 +0200

    Update Studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -1,25 +1,39 @@
 #include "Studentas.h"
 #include <iomanip>
 
-Studentas::Studentas(const Studentas& other){
-    vardas_ = other.vardas_;
-    pavarde_ = other.pavarde_;
-    nd_ = other.nd_;
-    egzaminas_ = other.egzaminas_;
-    galVid_ = other.galVid_;
-    galMed_ = other.galMed_;
+Studentas::Studentas(const Studentas& other)
+    : Zmogus(other),
+    nd_(other.nd_),
+    egzaminas_(other.egzaminas_),
+    galVid_(other.galVid_),
+    galMed_(other.galMed_){
 }
 
 Studentas& Studentas::operator=(const Studentas& other){
     if(this != &other){
-        vardas_ = other.vardas_;
-        pavarde_ = other.pavarde_;
+        Zmogus::operator=(other);
         nd_ = other.nd_;
         egzaminas_ = other.egzaminas_;
         galVid_ = other.galVid_;
         galMed_ = other.galMed_;
     }
     return *this;
+}
+
+Studentas::~Studentas(){
+    nd_.clear();
+}
+
+void Studentas::spausdintiInfo(std::ostream& os) const{
+    os << std::left << std::setw(15) << vardas_
+       << std::left << std::setw(20) << pavarde_
+       << std::fixed << std::setprecision(2)
+       << std::left << std::setw(16) << galVid_
+       << std::left << std::setw(16) << galMed_;
+}
+
+void Studentas::skaitytiInfo(std::istream& is){
+    readStudent(is);
 }
 
 double Studentas::galBalas(double (*skaiciavimas)(std::vector<int>&)) const{
@@ -29,36 +43,21 @@ double Studentas::galBalas(double (*skaiciavimas)(std::vector<int>&)) const{
 }
 
 std::istream& Studentas::readStudent(std::istream& is){
-     std::string v, p;
-    if(!(is >> v >> p)) return is;
-    vardas_ = v;
-    pavarde_ = p;
-    
-    int ndCount;
-    if(!(is >> ndCount)){
-        is.setstate(std::ios::failbit);
-        return is;
-    }
-    if(ndCount < 0) ndCount = 0;
+    is>>vardas_>>pavarde_;
     nd_.clear();
-     for(int i=0; i<ndCount; ++i){
-        int paz;
-        if(!(is >> paz)){
-            is.setstate(std::ios::failbit);
+    int paz;
+    for(int i=0; i<10; i++){
+        if(!(is>>paz)){
             return is;
         }
-        skaiciuotiGalutinis();
+        nd_.push_back(paz);
+    }
+    if(!(is>>egzaminas_)){
         return is;
     }
 
-    std::ostream& operator<<(std::ostream& os, const Studentas& s){
-    os<<std::left<<std::setw(15)<<s.vardas_
-      <<std::left<<std::setw(20)<<s.pavarde_
-      <<std::fixed<<std::setprecision(2)
-      <<std::left<<std::setw(16)<<s.galVid_
-      <<std::left<<std::setw(16)<<s.galMed_;
-      return os;
-        
+   skaiciuotiGalutinis();
+    return is;
 }
 
 void Studentas::skaiciuotiGalutinis(){
@@ -66,17 +65,20 @@ void Studentas::skaiciuotiGalutinis(){
         galVid_=galMed_=0.6*egzaminas_;
         return;
     }
-    double vid = 0.0;
+    double vid=0;
     for(int paz: nd_) vid+=paz;
-    vid /= static_cast<double>(nd_.size());
-    galVid_ = vid*0.4+0.6*egzaminas_;
-    galMed_ = median(const_cast<std::vector<int>&>(nd_))*0.4+0.6*egzaminas_;
+    vid/=nd_.size();
+    galVid_=vid*0.4+0.6*egzaminas_;
+    galMed_=median(nd_)*0.4+0.6*egzaminas_;
 }
 
-double Studentas::galBalas(double(*skaiciavimas)(std::vector<int>&)) const{
-    if (nd_.empty()) return 0.6 * egzaminas_;
-    std::vector<int> kopija = nd_;
-    return skaiciavimas(kopija) * 0.4 + 0.6 * egzaminas_;
+std::ostream& operator<<(std::ostream& os, const Studentas& s){
+    s.spausdintiInfo(os);
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Studentas& s){
+    return s.readStudent(is);
 }
 
 bool compare(const Studentas& a, const Studentas& b){
@@ -90,3 +92,4 @@ bool comparePagalPavarde(const Studentas& a, const Studentas& b){
 bool comparePagalEgza(const Studentas& a, const Studentas& b){
     return a.egzaminas() < b.egzaminas();
 }
+
