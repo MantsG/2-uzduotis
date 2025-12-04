488c798c88e7ac5ea1ccfae9f29a0310c6cd93d6
MantsG <pmantas05@gmail.com>
2025-12-04 16:18:50 +0200

    Update Studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -29,20 +29,36 @@ double Studentas::galBalas(double (*skaiciavimas)(std::vector<int>&)) const{
 }
 
 std::istream& Studentas::readStudent(std::istream& is){
-    is>>vardas_>>pavarde_;
-    nd_.clear();
-    int paz;
-    for(int i=0; i<10; i++){
-        if(!(is>>paz)){
-            return is;
-        }
-        nd_.push_back(paz);
-    }
-    if(!(is>>egzaminas_)){
+     std::string v, p;
+    if(!(is >> v >> p)) return is;
+    vardas_ = v;
+    pavarde_ = p;
+    
+    int ndCount;
+    if(!(is >> ndCount)){
+        is.setstate(std::ios::failbit);
         return is;
     }
-    skaiciuotiGalutinis();
-    return is;
+    if(ndCount < 0) ndCount = 0;
+    nd_.clear();
+     for(int i=0; i<ndCount; ++i){
+        int paz;
+        if(!(is >> paz)){
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        skaiciuotiGalutinis();
+        return is;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const Studentas& s){
+    os<<std::left<<std::setw(15)<<s.vardas_
+      <<std::left<<std::setw(20)<<s.pavarde_
+      <<std::fixed<<std::setprecision(2)
+      <<std::left<<std::setw(16)<<s.galVid_
+      <<std::left<<std::setw(16)<<s.galMed_;
+      return os;
+        
 }
 
 void Studentas::skaiciuotiGalutinis(){
@@ -50,24 +66,17 @@ void Studentas::skaiciuotiGalutinis(){
         galVid_=galMed_=0.6*egzaminas_;
         return;
     }
-    double vid=0;
+    double vid = 0.0;
     for(int paz: nd_) vid+=paz;
-    vid/=nd_.size();
-    galVid_=vid*0.4+0.6*egzaminas_;
-    galMed_=median(nd_)*0.4+0.6*egzaminas_;
+    vid /= static_cast<double>(nd_.size());
+    galVid_ = vid*0.4+0.6*egzaminas_;
+    galMed_ = median(const_cast<std::vector<int>&>(nd_))*0.4+0.6*egzaminas_;
 }
 
-std::ostream& operator<<(std::ostream& os, const Studentas& s){
-    os<<std::left<<std::setw(15)<<s.vardas_
-      <<std::left<<std::setw(20)<<s.pavarde_
-      <<std::fixed<<std::setprecision(2)
-      <<std::left<<std::setw(16)<<s.galVid_
-      <<std::left<<std::setw(16)<<s.galMed_;
-      return os;
-}
-
-std::istream& operator>>(std::istream& is, Studentas& s){
-    return s.readStudent(is);
+double Studentas::galBalas(double(*skaiciavimas)(std::vector<int>&)) const{
+    if (nd_.empty()) return 0.6 * egzaminas_;
+    std::vector<int> kopija = nd_;
+    return skaiciavimas(kopija) * 0.4 + 0.6 * egzaminas_;
 }
 
 bool compare(const Studentas& a, const Studentas& b){
@@ -81,10 +90,3 @@ bool comparePagalPavarde(const Studentas& a, const Studentas& b){
 bool comparePagalEgza(const Studentas& a, const Studentas& b){
     return a.egzaminas() < b.egzaminas();
 }
-
-
-
-
-
-
-
