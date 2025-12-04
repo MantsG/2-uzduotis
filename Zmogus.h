e137a1d5e9c81dd029ad118288c6ac3a1b5e1723
MantsG <pmantas05@gmail.com>
2025-12-04 16:36:42 +0200

    Update Zmogus.h

--- Zmogus.h
+++ Zmogus.h
@@ -1,63 +1,30 @@
-#pragma once
-#include "Zmogus.h"
-#include <iostream>
-#include <vector>
-#include <list>
+#ifndef ZMOGUS_H_INCLUDED
+#define ZMOGUS_H_INCLUDED
+
 #include <string>
-#include <numeric>
-#include <algorithm>
-#include "Utils.h"
+#include <iostream>
 
-//#define NAUDOTI_VECTOR
-#define NAUDOTI_LIST
-
-#ifdef NAUDOTI_LIST
-    using Konteineris = std::list<class Studentas>;
-#else
-    using Konteineris = std::vector<class Studentas>;
-#endif
-
-class Studentas : public Zmogus{
-private:
-    std::vector <int> nd_;
-    int egzaminas_ = 0;
-    double galVid_ = 0.0;
-    double galMed_ = 0.0;
+class Zmogus{
+protected:
+    std::string vardas_;
+    std::string pavarde_;
 
 
 public:
-    
-    Studentas() = default;
-    Studentas(std::istream& is){read(is);}
-    Studentas(const Studentas& other) = default;
-    Studentas& operator=(const Studentas& other) = default;
-    
-    ~Studentas() = default;
+    Zmogus() = default;
+    Zmogus(const std::string& vardas, const std::string& pavarde)
+        : vardas_(vardas), pavarde_(pavarde) {}
 
-    const std::vector<int>& nd() const {return nd_;}
-    int egzaminas() const {return egzaminas_;}
-    double galVid() const {return galVid_;}
-    double galMed() const {return galMed_;}
+    virtual ~Zmogus() = default;
 
-    void setEgzaminas(int e) {egzaminas_ = e;}
-    void setNd(const std::vector<int>& n) {nd_ = n;}
-    void addNd(int paz) {nd_.push_back(paz);}
+    std::string vardas() const {return vardas_;}
+    std::string pavarde() const {return pavarde_;}
 
-    void skaiciuotiGalutinis();
+    void setVardas(const std::string& v) {vardas_ = v;}
+    void setPavarde(const std::string& p) {pavarde_ = p;}
 
-    double galBalas(double (*skaiciavimas)(std::vector<int>&) = median) const;
-
-    std::ostream& write(std::ostream& os) const override;
-    std::istream& read(std::istream& is) override;
-
-    friend std::ostream& operator<<(std::ostream& os, const Studentas& s){
-        return s.write(os);
-    }
-    friend std::istream& operator>>(std::istream& is, Studentas& s){
-        return s.read(is);
-    }
+    virtual std::ostream& write(std::ostream& os) const = 0;
+    virtual std::istream& read(std::istream& is) = 0;
 };
 
-bool compare(const Studentas& a, const Studentas& b);
-bool comparePagalPavarde(const Studentas& a, const Studentas& b);
-bool comparePagalEgza(const Studentas& a, const Studentas& b);
+#endif // ZMOGUS_H_INCLUDED
