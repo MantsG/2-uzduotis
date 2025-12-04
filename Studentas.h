83790c9c24fd1cb285140c2100455a98b1ad0e44
MantsG <pmantas05@gmail.com>
2025-12-04 16:55:51 +0200

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "Zmogus.h"
 #include <iostream>
 #include <vector>
 #include <list>
@@ -17,45 +16,76 @@
     using Konteineris = std::vector<class Studentas>;
 #endif
 
+class Zmogus{
+protected:
+    std::string vardas_;
+    std:: string pavarde_;
+
+public:
+    Zmogus() = default;
+    Zmogus(const std::string& vardas, const std::string& pavarde)
+        : vardas_(vardas), pavarde_(pavarde) {}
+
+    virtual ~Zmogus() = default;
+
+    virtual void spausdintiInfo(std::ostream& os) const = 0;
+    virtual void skaitytiInfo(std::istream& is) = 0;
+
+    inline std::string vardas() const { return vardas_; }
+    inline std::string pavarde() const { return pavarde_; }
+
+    inline void setVardas(const std::string& v) { vardas_ = v; }
+    inline void setPavarde(const std::string& p) { pavarde_ = p; }
+
+    virtual Zmogus& operator=(const Zmogus& other){
+        if (this != &other){
+            vardas_ = other.vardas_;
+            pavarde_ = other.pavarde_;
+        }
+        return *this;
+    }
+};
+
 class Studentas : public Zmogus{
 private:
-    std::vector <int> nd_;
-    int egzaminas_= 0;
-    double galVid_ = 0.0;
-    double galMed_= 0.0;
+    std::vector<int> nd_;
+    int egzaminas_;
+    double galVid_;
+    double galMed_;
 
 
 public:
+    Studentas() : egzaminas_(0), galVid_(0), galMed_(0) {}
+    Studentas(const std::string& vardas, const std::string& pavarde)
+        : Zmogus(vardas, pavarde), egzaminas_(0), galVid_(0), galMed_(0) {}
+    Studentas(std::istream& is) { readStudent(is); }
 
-    Studentas()= default;
-    Studentas(std::istream& is){read(is);}
-    Studentas(const Studentas& other) = default;
-    Studentas& operator=(const Studentas& other) = default;
+    Studentas(const Studentas& other);
 
-    ~Studentas() = default;
+    Studentas& operator=(const Studentas& other);
 
-    const std::vector<int>& nd() const {return nd_;}
-    int egzaminas() const {return egzaminas_;}
-    double galVid() const {return galVid_;}
-    double galMed() const {return galMed_;}
+    ~Studentas() override;
 
-    void setEgzaminas(int e) {egzaminas_ = e;}
-    void setNd(const std::vector<int>& n) {nd_ = n;}
-    void addNd(int paz) {nd_.push_back(paz);}
+    void spausdintiInfo(std::ostream& os) const override;
+    void skaitytiInfo(std::istream& is) override;
 
-    void skaiciuotiGalutinis();
+    inline double galVid() const { return galVid_; }
+    inline double galMed() const { return galMed_; }
+    inline int egzaminas() const { return egzaminas_; }
+    inline const std::vector<int>& nd() const { return nd_; }
+
+    inline void setEgzaminas(int e) { egzaminas_ = e; }
+    inline void setNd(const std::vector<int>& n) { nd_ = n; }
+    inline void addNd(int paz) { nd_.push_back(paz); }
+
+    void pridetiNd(int pazymys) { nd_.push_back(pazymys); }
 
     double galBalas(double (*skaiciavimas)(std::vector<int>&) = median) const;
+    std::istream& readStudent(std::istream& is);
+    void skaiciuotiGalutinis();
 
-    std::ostream& write(std::ostream& os) const override;
-    std::istream& read(std::istream& is) override;
-
-     friend std::ostream& operator<<(std::ostream& os, const Studentas& s){
-        return s.write(os);
-    }
-    friend std::istream& operator>>(std::istream& is, Studentas& s){
-        return s.read(is);
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
+    friend std::istream& operator>>(std::istream& is, Studentas& s);
 };
 
 bool compare(const Studentas& a, const Studentas& b);
